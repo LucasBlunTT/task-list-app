adffbcaad0b41eee88429b38dc8ae3ed3e855a50
Lucas da Silva <sklucassilva@gmail.com>
2024-09-18 13:28:04 -0300

    Update tab layout to display a message when there are no unread messages

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,18 +1,10 @@
-import { FlatList, Text, View } from 'react-native';
-import { Input } from '@/components/input/input';
-import { MenuButton } from '@/components/menuButton/menu-button';
-import { Avatar } from '@/components/avatar/avatar';
-import { Email } from '@/components/email/email';
-import { EMAILS } from '@/utils/email';
-import FloatButton from '@/components/floatButton/floatButton';
-import { useNavigation } from '@react-navigation/native';
-
+import { Text, View } from 'react-native';
 export default function index() {
-  const navigation = useNavigation();
-
   return (
-    <View className="flex-1 bg-gray-900 pt-14 p-4">
-     
+    <View className="flex-1 bg-gray-900 pt-14 p-4 items-center justify-center">
+      <Text className="color-white text-lg">
+        Nao há mensagens para serem lidas. ‌
+      </Text>
     </View>
   );
 }
