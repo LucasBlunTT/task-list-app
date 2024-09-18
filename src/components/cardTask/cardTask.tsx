d18e554d10d2a97a244728a4783b54c9d1bc9260
Lucas da Silva <sklucassilva@gmail.com>
2024-09-18 17:54:31 -0300

    Refactorizar nomes de componentes, corrigir formatação de código e adicionar funcionalidade de riscar tarefa concluída

--- src/components/cardTask/cardTask.tsx
+++ src/components/cardTask/cardTask.tsx
@@ -10,14 +10,20 @@ type CardTaskProps = {
 
 const CardTask = ({ taskName, taskTime, complete }: CardTaskProps) => {
   const [check, setCheck] = useState(complete);
-  
+
   return (
     <View
       className="bg-[#E2EBFA] p-4 rounded-2xl flex-row items-center justify-between"
       style={{ width: '85%' }}
     >
       <View>
-        <Text className="text-sm font-bold">{taskName}</Text>
+        <Text
+          className={`${
+            check ? 'line-through' : 'no-underline'
+          } text-sm font-bold`}
+        >
+          {taskName}
+        </Text>
         <Text className="text-xs text-slate-400">{taskTime}</Text>
       </View>
       <TouchableOpacity>
